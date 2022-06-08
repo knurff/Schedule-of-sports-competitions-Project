be8868af13aa3dd265540beed2a2b66c087b9666
knurff <91338352+knurff@users.noreply.github.com>
2022-06-09 02:39:09 +0300

    fix GameValidator

--- src/main/java/com/example/sport_schedule/validation/game/DateValidator.java
+++ src/main/java/com/example/sport_schedule/validation/game/DateValidator.java
@@ -20,8 +20,8 @@ public class DateValidator implements ConstraintValidator<DateValidation, Game> 
 
     @Override
     public boolean isValid(Game game, ConstraintValidatorContext constraintValidatorContext) {
-        if (game.getId() != null && game.getDate() != null) {
-            if (game.getDate().isBefore(LocalDateTime.now()))
+        if (game.getDate() != null) {
+            if (game.getId() != null && game.getDate().isBefore(LocalDateTime.now()))
                 return gameService.getById(game.getId()).getDate().isEqual(game.getDate());
             return game.getDate().isAfter(LocalDateTime.now());
         }
